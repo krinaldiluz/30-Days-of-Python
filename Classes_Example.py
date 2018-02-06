fe3568ca8439128439627106e6039d5527a7e771
Kelvin Rinaldi <34136901+krinaldiluz@users.noreply.github.com>
2018-02-05 22:29:42 -0200

    Update Classes_Example.py

--- Classes_Example.py
+++ Classes_Example.py
@@ -1,10 +1,12 @@
 class Animal():
+	name = 'Amy'
 	noise = "Grunt"
 	size = "Large"
 	color = "brown"
 	hair = "covers body"
 	def get_color(self):
 		return self.color
+	@property	
 	def make_noise(self):	
 		return self.noise
 
@@ -24,5 +26,18 @@ jon = Dog()
 jon.color = 'white'
 jon. name = "Jon Snow"
 
+
+dog = Animal()
+dog.get_color()
+
+dog.make_noise
+
+
+email_address = "another@gmail.com"
+to_list = ['abc@gmail.com']
+from_list = ['another2@gmail.com', 'hello@teamcfe.com']
+
+def send_email(email=email_address, to_list=to_list, from_list=from_list):
+	pass
 
 
