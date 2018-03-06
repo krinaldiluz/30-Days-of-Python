654f3925c7cf1e08099888f491317fddb4107868
Kelvin Rinaldi <34136901+krinaldiluz@users.noreply.github.com>
2018-03-05 21:33:13 -0300

    Update hungry_data.py

--- CSV_Examples/hungry_data.py
+++ CSV_Examples/hungry_data.py
@@ -1,5 +1,7 @@
 import os
 import csv
+import shutil
+from tempfile import NamedTemporaryFile
 
 os.getcwd()
 os.chdir('C:\\Users\\Home\\Documents\\Material de Estudo\\Python\\Curso 30 Days of Python\\CSV_Examples')
@@ -26,4 +28,26 @@ def append_data(file_path, name, email):
 
 
 
-append_data("data.csv", "Justin", "hello@teamcfe.com")
+#append_data("data.csv", "Justin", "hello@teamcfe.com")
+
+
+filename = "data.csv"
+temp_file = NamedTemporaryFile(delete=False)
+
+with open(filename, "rb") as csvfile, temp_file:
+    reader = csv.DictReader(csvfile)
+    filename = ['id','name', 'email', 'amount', 'sent']
+    writer = csv.DictWriter(temp_file, fieldnames=fieldnames)
+    writer.writerheader()
+
+    for row in reader:
+        print(row)
+        writer.writerow({
+                "id": row["id"],
+                "name": row["name"],
+                "email": row["email"],
+                "amount": "1293.23",
+                "sent": ""
+            })
+
+#shutil.move(temp_file, filename)
